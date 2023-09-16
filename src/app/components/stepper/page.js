e4e1e79aebaef8010da62ea3475e5234a64d9b06
Salman Bukhari <salman.bukhari@outgive.ca>
2023-09-16 20:56:31 +0500

    frontend

---
+++ src/app/components/stepper/page.js
@@ -0,0 +1,29 @@
+'use client'
+import React from 'react'
+// import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = [
+  'Select master blaster campaign settings',
+  'Create an ad group',
+  'Create an ad',
+];
+const page = () => {
+  return (
+<>
+<Box className="mt-20" sx={{ width: '100%' }}>
+      <Stepper activeStep={1} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+</>  )
+}
+
+export default page
